58edf15d9fe4af411436dc9dd1addeed68656cb6
ilyongHwang <iyhwang@hnmcorp.kr>
2021-01-02 20:54:47 +0900

    3.7 Update Restaurant part One

--- nuber-eats-backend/src/restaurants/restaurants.resolver.ts
+++ nuber-eats-backend/src/restaurants/restaurants.resolver.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantService } from './restaurants.service';
 
@@ -28,5 +29,16 @@ export class RestaurantResolver {
       console.log(err);
       return false;
     }
+  }
+
+  @Mutation(returns => Boolean)
+  async updateRestaurant(
+    @Args('input') updateRestaurantDto: UpdateRestaurantDto
+    /*
+    @Args('id') id: number,
+    @Args('data') data: UpdateRestaurantDto,
+    */
+  ): Promise<boolean> {
+    return true;
   }
 }
