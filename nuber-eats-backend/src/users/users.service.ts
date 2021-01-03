6ce04a89c8ec7e54a33bf7e0c36875674f33bea2
ilyongHwang <iyhwang@hnmcorp.kr>
2021-01-04 01:13:09 +0900

    #4.4 Create Account Mutation part Two

--- nuber-eats-backend/src/users/users.service.ts
+++ nuber-eats-backend/src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { CreateAccountOutput } from "./dtos/create-account.dto";
 import { User } from "./entities/user.entity";
 
 @Injectable()
@@ -10,4 +11,22 @@ export class UserService {
     private readonly users: Repository<User>
   ) { }
 
+  async createAccount({ email, password, role }: CreateAccountOutput) {
+    try {
+      // 1. check new user
+      const exists = await this.users.findOne({ email });
+      if (exists) {
+        // make Error
+        return;
+      }
+
+      // 2. create user
+      // 2-1hash the password
+      await this.users.save(this.users.create({ email, password, role }))
+
+    } catch (err) {
+
+    }
+
+  }
 }
